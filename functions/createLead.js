ca3b2752fed23a6bdb8fcd7e3cc2fc5abf8bd10f
Allan <alexandesigner@gmail.com>
2020-11-16 14:51:36 -0300

    fix: createLead event post

--- functions/createLead.js
+++ functions/createLead.js
@@ -1,17 +1,24 @@
 const axios = require('axios')
 const { API_KEY } = process.env
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
 
+  const body = JSON.parse(event.body)
   const url = 'https://api.rd.services/platform/conversions'
 
-  const response = await axios.post(`${url}?api_key=${API_KEY}`, event.body)
-
-  return {
-    statusCode: 200,
-    body: response,
-  }
+  await axios({
+    method: 'post',
+    url: `${url}?api_key=${API_KEY}`,
+    headers: {
+      'content-type': 'application/json',
+    },
+    data: JSON.stringify(body),
+  })
+    .then((response) => {
+      return response.data
+    })
+    .catch((err) => callback(err))
 }

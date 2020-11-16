c6c3f9599d97fc96f59b5a2831781ae0b8d6fd3c
Allan <alexandesigner@gmail.com>
2020-11-16 16:56:55 -0300

    fix: return callback

--- functions/createLead.js
+++ functions/createLead.js
@@ -1,28 +1,23 @@
 const axios = require('axios')
 const { API_KEY } = process.env
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
 
-  const body = JSON.parse(event.body)
   const url = 'https://api.rd.services/platform/conversions'
 
-  axios({
+  await axios({
     method: 'post',
     url: `${url}?api_key=${API_KEY}`,
     headers: {
       'content-type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers':
-        'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Max-Age': '2592000',
-      'Access-Control-Allow-Credentials': 'true',
     },
-    data: JSON.stringify(body),
+    data: event.body,
   })
-    .then((response) => ({ statusCode: response.status, body: response.data }))
+    .then((response) =>
+      callback({ statusCode: response.status, body: response.data })
+    )
     .catch((err) => callback(err))
 }

73ab9e9456435716cd6f5aca6202e52f4b98d1e7
Allan <alexandesigner@gmail.com>
2020-11-16 13:21:04 -0300

    fix: remove callback

--- functions/createLead.js
+++ functions/createLead.js
@@ -1,13 +1,7 @@
 const axios = require('axios')
 
-exports.handler = function createLead(data, callback) {
+exports.handler = function createLead(data) {
   const url = `https://api.rd.services/platform/conversions`
   const apiKey = process.env.API_KEY
-
-  axios
-    .post(`${url}?api_key=${apiKey}`, data)
-    .then((response) => response)
-    .catch((e) => {
-      callback(e)
-    })
+  axios.post(`${url}?api_key=${apiKey}`, data).then((response) => response)
 }

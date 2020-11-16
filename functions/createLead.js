6fde854f40fef7f94ad5cd96e7d01e8ab5090ae8
Allan <alexandesigner@gmail.com>
2020-11-16 15:51:16 -0300

    fix: statusCode and body response

--- functions/createLead.js
+++ functions/createLead.js
@@ -1,24 +1,21 @@
 const axios = require('axios')
 const { API_KEY } = process.env
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' }
   }
 
-  const body = JSON.parse(event.body)
   const url = 'https://api.rd.services/platform/conversions'
 
-  axios({
+  const response = await axios({
     method: 'post',
     url: `${url}?api_key=${API_KEY}`,
     headers: {
       'content-type': 'application/json',
     },
-    data: JSON.stringify(body),
+    data: JSON.stringify(event.body),
   })
-    .then((response) => {
-      return response
-    })
-    .catch((err) => callback(err))
+
+  return { statusCode: response.status, body: response.data }
 }
